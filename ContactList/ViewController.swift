1c179c08ffdb2cf7492dd83407f27634ab01b7da
Samir Dzhafari <samirjafari2001@icloud.com>
2023-10-23 19:58:21 +0300

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Самир Джафари on 23.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
